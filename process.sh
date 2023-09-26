dd87d55c13d3ccb09fb782a7af8a37cf51fe9ce4
Jamil Dhanani <jamil.dhanani@gmail.com>
2023-09-26 16:20:41 +0000

    Add 4_0 quantization

--- process.sh
+++ process.sh
@@ -4,4 +4,5 @@ python merge_peft_adapters.py
 mv merged_peft $MODEL_NAME
 python ../llama.cpp/convert.py $MODEL_NAME/final_merged_checkpoint/ --outtype f32 --outfile ../$MODEL_NAME.full.gguf
 python ../llama.cpp/convert.py $MODEL_NAME/final_merged_checkpoint/ --outtype q8_0 --outfile ../$MODEL_NAME.q8_0.gguf
-# ../llama.cpp/quantize ../$MODEL_NAME.full.gguf ../$MODEL_NAME.q4_0.gguf q4_0
+python ../llama.cpp/convert.py $MODEL_NAME/final_merged_checkpoint/ --outtype q4_0 --outfile ../$MODEL_NAME.q4_0.gguf
+../llama.cpp/quantize ../$MODEL_NAME.full.gguf ../$MODEL_NAME.q4_0.gguf q4_0
